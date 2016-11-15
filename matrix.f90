f2cd2938d59b796363e450f3ef7865c25a36eb98
J.R. Powers-Luhn <floobyt@gmail.com>
2016-11-14 22:13:14 -0500

    started work on matrix

--- matrix.f90
+++ matrix.f90
@@ -1,39 +1,52 @@
-program matrix
-! Neutron Flux desperation Group 3
-! DGETRS - solve a system of linear equations A * X = B or 
-! See: http://www.netlib.org/lapack/explore-html/d6/d49/dgetrs_8f.html
+function matrix(nodes, sigma_a, sigma_inscatter, sigma_outscatter)
 
-use iso_fortran_env
 implicit none
+integer                     :: nodes
+real(real64), allocatable   :: A(:,:)
+real(real64), allocatable   :: ipiv(:,:)
+real(real64)                :: sigma_a, sigma_inscatter, sigma_outscatter
+real(real64)                :: D
+real(real64)                :: w
+real(real64)                :: dx, dx2 
 
-real(real64), allocatable :: A(:,:)                 ! matrix to LU decompose
-real(real64), allocatable :: b(:)                   ! b vector
-real(real64), allocatable :: ipiv(:)                ! pivoting array for LAPACK
+end function matrix
 
-!--------------------------Matrix Declaration-----------------------------!
+! subroutine matrix
+! ! Neutron Flux desperation Group 3
+! ! DGETRS - solve a system of linear equations A * X = B or 
+! ! See: http://www.netlib.org/lapack/explore-html/d6/d49/dgetrs_8f.html
 
-A(1,1) = (0.5*sigma_a)+(D/(dx**2))
-A(1,2) = -D/(dx**2.0)
-A(n,n) = sigma_a+2.0*D/(dx**2)
-A(n,n-1) = -D/(dx**2)
+! use iso_fortran_env
+! implicit none
 
-do i=2 , n-1
+! real(real64), allocatable :: A(:,:)                 ! matrix to LU decompose
+! real(real64), allocatable :: b(:)                   ! b vector
+! real(real64), allocatable :: ipiv(:)                ! pivoting array for LAPACK
 
-    A(i,i) = sigma_a+2.0*D/(dx**2)
-    A(i,i+1) = -D/(dx**2)
-    A(i,i-1) = -D/(dx**2)
+! !--------------------------Matrix Declaration-----------------------------!
 
-enddo
+! A(1,1) = (0.5*sigma_a)+(D/(dx**2))
+! A(1,2) = -D/(dx**2.0)
+! A(n,n) = sigma_a+2.0*D/(dx**2)
+! A(n,n-1) = -D/(dx**2)
 
-! Pretty matrix printing
-! From http://jblevins.org/log/array-write
-! do i=1,n
-!     write(*,"(100g15.5)") ( A(i,j), j=1,n )
+! do i=2 , n-1
+
+!     A(i,i) = sigma_a+2.0*D/(dx**2)
+!     A(i,i+1) = -D/(dx**2)
+!     A(i,i-1) = -D/(dx**2)
+
 ! enddo
 
-! LU factorization of a general M-by-N matrix A
-! See: http://www.netlib.org/lapack/explore-html/d3/d6a/dgetrf_8f.html
-call DGETRF(n, n, A, n, ipiv, info)
-if (info /= 0) stop 'Matrix is numerically singular!'
+! ! Pretty matrix printing
+! ! From http://jblevins.org/log/array-write
+! ! do i=1,n
+! !     write(*,"(100g15.5)") ( A(i,j), j=1,n )
+! ! enddo
 
-end program matrix
+! ! LU factorization of a general M-by-N matrix A
+! ! See: http://www.netlib.org/lapack/explore-html/d3/d6a/dgetrf_8f.html
+! call DGETRF(n, n, A, n, ipiv, info)
+! if (info /= 0) stop 'Matrix is numerically singular!'
+
+! end subroutine matrix
