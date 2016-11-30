cb1e3c8257850e2ee213014eda62f283cb2ffbcb
J.R. Powers-Luhn <jpowersl@vols.utk.edu>
2016-11-30 18:00:03 -0500

    fixed dumb linting errors

--- Project 3/python/babyreactor/qt.py
+++ Project 3/python/babyreactor/qt.py
@@ -2,7 +2,7 @@ from babyreactor.matrix import matrix
 import numpy as np
 
 
-absorption = 0.15
-scattering = np.array([0.15, 0.15, 0.15, 0.15])
-diffusion = 9.
-matrix(scattering, absorption, diffusion, 10, 5)
+ABSORPTION = 0.15
+SCATTERING = np.array([0.15, 0.15, 0.15, 0.15])
+DIFFUSION = 9.
+matrix(SCATTERING, ABSORPTION, DIFFUSION, 10, 5)
